d98f97b13fd6ef392eab3b1062ab57b38ad71dd7
gaspard <gaspard@gmail.com>
2013-04-15 16:16:49 +0200

    journal: ajout des squelettes pour le journal

--- squelettes/js/script.js
+++ squelettes/js/script.js
@@ -3,4 +3,8 @@ $(document).ready(function(){
 	$('body.blog h3.accessibility').css({cursor:'pointer'}).click(function () {
 		$(this).next().slideToggle();
 	});
+	
+	// clear spip_photo size, let it be responsive
+	$('.spip_photo img').attr({'width':'','height':'','style':''});
+
 });
