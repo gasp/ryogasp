412001e421f7c5d040510d0ea9cdc7e0f1fc02b4
gaspard <gaspard@gmail.com>
2013-05-23 17:26:24 +0200

    portfolio: bigger + raquo, click on it to see next slide

--- squelettes/js/journal.js
+++ squelettes/js/journal.js
@@ -5,23 +5,38 @@ $(document).ready(function(){
 	// add next and previous
 	$('.portfolio .spip_documents img').each(function(){
 		// get the link of the next one
-		var i=14, id='', cn = $(this).parent().parent()[0].className;
-		while(!isNaN(cn[i])){
-			id += cn[i];
-			i++;
-		};
 		
+		var i=14, // position of the number
+			id='', // empty string
+			cn = $(this).parent().parent()[0].className;
+
+		// go and fetch !
+		while(!isNaN(cn[i]))
+			id += cn[i++];
+
 		var el = $('.portfolio a#doc'+id);
 		if(!el.length) el = $('.portfolio a#img'+id);
 		
-		
-		var next = $('a',$(el).parent().next())[0] || {onclick:function(){return false}};
+		var next = $('a',$(el).parent().next())[0] || {onclick:function(){return false},href:false};
 		$(this).click(function(){
 			next.onclick();
 		}).css({'cursor':'pointer'})
-		
-	})
-	// maximize image size
 	
+		// maximize image size
+		var h = $(this).css({'position':'absolute','max-width':'90%', 'z-index':2}).height();
+		$(this).parent().css({'height':h});
 
+//		console.log(next.href);
+		if(next.href){
+			var raquo = $('<div>&raquo;</div>').addClass('raquo').click(function(){
+				next.onclick();
+			});
+			$(this).parent().mouseover(function(){
+				raquo.css({'visibility':'visible'});
+			}).mouseout(function(){
+				raquo.css({'visibility':'hidden', 'height':h});
+			}).append(raquo);
+			
+		}// there is no next
+	});
 });
