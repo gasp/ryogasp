15f652f476b56c297b445b62f7789c8a7e1fb2d9
gaspard <gaspard@gmail.com>
2015-02-26 19:09:55 +0100

    blog: give me peas

--- squelettes/js/blog.js
+++ squelettes/js/blog.js
@@ -2,4 +2,31 @@ $(document).ready(function(){
 	$('h3.accessibility',this).css({cursor:'pointer'}).click(function () {
 		$(this).next().slideToggle();
 	});
+
+	// petits pois
+	var $peas = $("<div><a href=\"#\"><span></span> petits pois</a></div>")
+		.attr("id", "peas");
+
+	$("#entete").append($peas);
+	$("#peas a").trigger("click");
+
+	var peas;
+	$.get( "http://api.cepcam.org:1907/bloggers/authors", function( data ) {
+		var blog = $.grep(data, function(v){
+			return v.slug == "ryoga";
+		})[0];
+		peas = blog.peas;
+		$("#peas a span").text(blog.peas);
+	});
+
+	$("#peas a").on("click", function(ev) {
+		$.post('http://api.cepcam.org:1907/bloggers/feed/ryoga', function() {
+			$("#peas a span").text(++peas);
+		});
+		ev.preventDefault();
+		return false;
+	});
+
+
+
 });
