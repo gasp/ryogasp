cc0edc4e6f843ac229b1a701b82007b4476aa3ae
gasp <gaspard@gmail.com>
2023-12-07 22:59:04 +0100

    feat: gzip dump

--- scripts/dump.sh
+++ scripts/dump.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 date=$(date '+%Y-%m-%d')
-docker exec -it ryogasp_spip_1 spip sql:dump:create --name $date
-
+docker exec ryogasp_spip_1 spip sql:dump:create --name $date
 docker exec ryogasp_mariadb_1 mariadb-dump --user=spip --password=spippassword spip > ./dump-$date.sql
+
+gzip -9 ../src/tmp/dump/$date.sqlite
+gzip -9 ./dump-$date.sql
+mv ./dump-$date.sql.gz ../src/tmp/dump/
 
